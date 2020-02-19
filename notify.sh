8a3fb88aaea8af1998e785e09adea528cf5f92a0
juniorxsound <fleisher.or@gmail.com>
2020-02-19 04:34:18 -0500

    Update notification msg

--- notify.sh
+++ notify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Blender Docker images deployed to Docker Hub :party:", "icon_emoji": ":blender-3d:"}' ${WEBHOOK_URL}
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Blender Docker images deployed to Docker Hub ":docker:" https://hub.docker.com/r/nytimes/rd-blender-docker", "username": "Blender Docker CI", "icon_emoji": ":blender-3d:"}' ${WEBHOOK_URL}
