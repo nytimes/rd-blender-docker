f6064474267e1b1a0f30b8a4b7e9d4abc0613aaf
juniorxsound <fleisher.or@gmail.com>
2020-05-01 11:36:15 -0400

    Copy over READMEs so we deploy them to Dockerhub in CI

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ Written by juniorxsound <https://orfleisher.com>
 
 import os
 import json
+import shutil
 import subprocess
 
 if __name__ == "__main__":
@@ -19,12 +20,21 @@ if __name__ == "__main__":
 
         for image in manifest["images"]:
             # Check if folder exists first
-            if (os.path.exists(output_folder + image["tag"])):
-                print('----------------------------------------------------------')
+            if os.path.exists(output_folder + image["tag"]):
+                print("----------------------------------------------------------")
+                print("🛠️ Building image nytimes/blender:{}".format(image["tag"]))
+
+                # Build the Docker image
+                os.system(
+                    "docker build ./ -f {}/Dockerfile -t nytimes/blender:{}".format(
+                        output_folder + image["tag"], image["tag"]
+                    )
+                )
+
+                # Copy the README from root so it deploys to DockerHub
+                shutil.copy2("./README.md", output_folder + image["tag"])
+
                 print(
-                    '🛠️ Building image nytimes/blender:{}'.format(image["tag"]))
-                os.system('docker build --quiet ./ -f {}/Dockerfile -t nytimes/blender:{}'.format(
-                    output_folder + image["tag"], image["tag"]))
-                print(
-                    '✔️ Built and tagged image nytimes/blender:{}'.format(image["tag"]))
-                print('----------------------------------------------------------\n')
+                    "✔️ Built and tagged image nytimes/blender:{}".format(image["tag"])
+                )
+                print("----------------------------------------------------------\n")
